5464a894c3dc48ee5a5583b2a742deaa6399126c
Natalie Savchenko <mel.absinthiatum@gmail.com>
2019-11-28 21:05:30 +0700

    Print Expected and Actual declarations using visitor

--- src/main/java/actions/psi/KotlinVisitorAction.kt
+++ src/main/java/actions/psi/KotlinVisitorAction.kt
@@ -6,7 +6,10 @@ import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiManager
+import org.jetbrains.kotlin.idea.util.isEffectivelyActual
+import org.jetbrains.kotlin.idea.util.isExpectDeclaration
 import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.psiUtil.isPublic
 
 class KotlinVisitorAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
@@ -28,12 +31,30 @@ class KotlinVisitorAction : AnAction() {
                 val psiF = PsiManager.getInstance(eventProject).findFile(it)
 
                 if (psiF != null && psiF.fileType.name == "Kotlin") {
-                    println("file name ${psiF.name}")
                     psiF.acceptChildren(object : KtTreeVisitorVoid() {
                         override fun visitNamedDeclaration(declaration: KtNamedDeclaration) {
-                            declaration.name?.let { declaredName ->
-                                println("Declaration name $declaredName")
-                                super.visitNamedDeclaration(declaration)
+                            val public = declaration.isPublic
+                            val expected = declaration.isExpectDeclaration()
+                            val actual = declaration.isEffectivelyActual()
+
+                            if (expected || actual) {
+                                println("file name ${psiF.name}")
+                                println("declaration: ${declaration.text}")
+
+                                when (declaration) {
+                                    is KtClass -> println("class")
+                                    is KtNamedFunction -> {
+                                        println("ssf ${declaration.bodyExpression}")
+                                        println("function")
+                                    }
+                                    is KtProperty -> println("property")
+                                }
+
+                                declaration.name?.let { declaredName ->
+                                    println("Declaration name $declaredName")
+
+                                    super.visitNamedDeclaration(declaration)
+                                }
                             }
                         }
                     })
