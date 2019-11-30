5624edcaac52627a39b63e31589879387881d07e
Natalie Savchenko <mel.absinthiatum@gmail.com>
2019-11-30 17:51:08 +0700

    Rough display shared elements tree after `smartInvokeLater`

--- src/main/java/abyss/toolWindow/MppToolWindow.kt
+++ src/main/java/abyss/toolWindow/MppToolWindow.kt
@@ -1,16 +1,16 @@
 package abyss.toolWindow
 
 import abyss.psi.SharedItemsProvider
+import abyss.view.AbyssTreeProvider
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.ui.components.JBScrollPane
-import com.intellij.ui.treeStructure.Tree
-import javax.swing.BorderFactory
-import javax.swing.BoxLayout
-import javax.swing.ImageIcon
-import javax.swing.JPanel
+import toolWindow.tree.TreeNodeContent
+import javax.swing.*
+import javax.swing.tree.DefaultMutableTreeNode
 import javax.swing.tree.DefaultTreeCellRenderer
 import javax.swing.tree.TreeCellRenderer
+import javax.swing.tree.TreeSelectionModel
 
 class MppToolWindow (private val project: Project, private val toolWindow: ToolWindow) {
     val content: JPanel
@@ -18,47 +18,48 @@ class MppToolWindow (private val project: Project, private val toolWindow: ToolW
     init {
         val panel = JPanel()
         panel.layout = BoxLayout(panel, BoxLayout.LINE_AXIS)
-        val customTree = createToolWindowTree()
-
-        val scrollPane = JBScrollPane(customTree)
-
-        scrollPane.border = BorderFactory.createEmptyBorder()
-        panel.add(scrollPane)
+        createToolWindowTree()
+//        val customTree = createToolWindowTree()
+//
+//        val scrollPane = JBScrollPane(customTree)
+//
+//        scrollPane.border = BorderFactory.createEmptyBorder()
+//        panel.add(scrollPane)
 
         content = panel
     }
 
-    private fun createToolWindowTree(): Tree {
-        val treeModel = SharedItemsProvider().getSharedItems(project) { treeModel ->
+    private fun createToolWindowTree() {
+        SharedItemsProvider().getSharedItems(project) { treeModel ->
+            val toolWindowTree = AbyssTreeProvider().tree(treeModel)
 
+            toolWindowTree.run {
+                isRootVisible = false
+                selectionModel.selectionMode = TreeSelectionModel.SINGLE_TREE_SELECTION
+                cellRenderer = makeTreeCellRenderer()
+
+                addTreeSelectionListener { event ->
+                    val source = event.source as JTree
+                    val node = source.lastSelectedPathComponent as DefaultMutableTreeNode?
+                    val obj = node?.userObject as TreeNodeContent?
+                    obj?.action?.invoke()
+                }
+            }
+
+            val scrollPane = JBScrollPane(toolWindowTree)
+
+            scrollPane.border = BorderFactory.createEmptyBorder()
+            content.add(scrollPane)
         }
-        // TODO: use coroutines and for UI use activity indicator.
-
-//        val toolWindowTree = AbyssTreeProvider().tree(treeModel)
-//
-//        toolWindowTree.run {
-//            isRootVisible = false
-//            selectionModel.selectionMode = TreeSelectionModel.SINGLE_TREE_SELECTION
-//            cellRenderer = makeTreeCellRenderer()
-//
-//            addTreeSelectionListener { event ->
-//                val source = event.source as JTree
-//                val node = source.lastSelectedPathComponent as DefaultMutableTreeNode?
-//                val obj = node?.userObject as TreeNodeContent?
-//                obj?.action?.invoke()
-//            }
-//        }
-//        return toolWindowTree
-        return Tree()
+        // TODO: use coroutines and for UI use an activity indicator.
     }
 
     private fun makeTreeCellRenderer(): TreeCellRenderer {
-        val imageIcon = ImageIcon(javaClass.getResource("/abyss/Obj.png"))
+        val imageIcon = ImageIcon(javaClass.getResource("/abyss/class.png"))
         val renderer = DefaultTreeCellRenderer()
         renderer.leafIcon = imageIcon
         renderer.borderSelectionColor = null
         renderer.backgroundSelectionColor = null
         return renderer
     }
-
 }
