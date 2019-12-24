219406ca68b876270f3ea3970c40f172982ea741
Natalie Savchenko <mel.absinthiatum@gmail.com>
2019-12-24 15:16:08 +0700

    Test mutations on the root children level, fix mutations tree

--- src/main/java/abyss/model/tree/diff/TreesDiffManager.kt
+++ src/main/java/abyss/model/tree/diff/TreesDiffManager.kt
@@ -1,68 +1,37 @@
 package abyss.model.tree.diff
 
 import abyss.model.tree.nodes.CustomNodeInterface
-import abyss.model.tree.nodes.NodeInterface
 import javax.swing.tree.DefaultMutableTreeNode
 import javax.swing.tree.TreeNode
 
 class TreesDiffManager {
-    fun diffs(oldTree: NodeInterface, newTree: NodeInterface) {
-        newTree.children().toList()
+
+    fun makeMutationsTree(oldNode: CustomNodeInterface, newNode: CustomNodeInterface): DefaultMutableTreeNode? {
+        return makeMutationsNode(oldNode,newNode)
     }
 
-    private fun compareNodes(oldNode: NodeInterface, newNode: NodeInterface): List<TreeMutation> {
-        val oldChildren = oldNode.children().toList()
-        val newChildren = newNode.children().toList()
-        val addedChildren = newChildren.subtract(oldChildren)
-        val removedChildren = oldChildren.subtract(newChildren)
-        val insertMutations = addedChildren.map { Insert(it, oldNode) }
-        val removeMutations = removedChildren.map { Remove(it, oldNode) }
-
-//        val otherMutations = compareNodes()
-        return insertMutations + removeMutations
-    }
-
-
-    private fun v(oldNode: NodeInterface, newNode: NodeInterface): TreeNode? {
-        // check mutations by subtract
-        // make a collection of mutations
-
-        // find intersect children array
-        // run on each child, return node with mutations, ancestor of mutated node or null
-        // we got a list of children or null
-
-
-        // if we got null of children and null of mutations return null
-
-        // return a node with a pointer to oldNode model and collection of mutations
-        return null
-
-    }
-
-    private fun f(oldNode: CustomNodeInterface, newNode: CustomNodeInterface): DefaultMutableTreeNode? {
+    private fun makeMutationsNode(oldNode: CustomNodeInterface, newNode: CustomNodeInterface): DefaultMutableTreeNode? {
+        if (oldNode.isLeaf && newNode.isLeaf) {
+            return null
+        }
         val oldChildren = oldNode.childNodes().toList()
         val newChildren = newNode.childNodes().toList()
-        val parent = oldNode.nodeParent()
-        val model = oldNode.nodeModel()
 
-        if (parent == null) {
-            return null
-        }
-        if (model == null) {
-            return null
-        }
+        val model = oldNode.nodeModel() ?: return null
 
-        val result = sift(oldChildren, newChildren, parent)
+        val result = sift(oldChildren, newChildren, oldNode)
         val mutations = result.mutations
         val unchanged = result.unchanged
 
-        val nodes = unchanged.mapNotNull { f(it.oldNode, it.newNode) }
+        val nodes = unchanged.mapNotNull { makeMutationsNode(it.oldNode, it.newNode) }
 
         if (mutations.isEmpty() && nodes.isEmpty()) {
             return null
         }
         val diffNodeModel = DiffNodeModel(model, mutations)
-        return DefaultMutableTreeNode(diffNodeModel)
+        val resultNode = DefaultMutableTreeNode(diffNodeModel)
+        nodes.forEach { resultNode.add(it) }
+        return resultNode
     }
 
     private fun sift(
@@ -73,16 +42,17 @@ class TreesDiffManager {
         val mutableNewChildren = newChildren.toMutableList()
         val mutableOldChildren = oldChildren.toMutableList()
         val unchangedNodesTuples = mutableListOf<NodesTuple>()
-        for (oldChild in mutableOldChildren) {
-            for (newChild in mutableNewChildren) {
-                if (oldChild.nodeModel() == newChild.nodeModel()) {
-                    unchangedNodesTuples.add(NodesTuple(oldChild, newChild))
-                    mutableOldChildren.remove(oldChild)
-                    mutableNewChildren.remove(newChild)
-                    continue
-                }
+        for (oldChild in oldChildren) {
+            val matchedNode = mutableNewChildren.findLast { newChild ->
+                oldChild.nodeModel() == newChild.nodeModel()
+            }
+            if (matchedNode != null) {
+                unchangedNodesTuples.add(NodesTuple(oldChild, matchedNode))
+                mutableOldChildren.remove(oldChild)
+                mutableNewChildren.remove(matchedNode)
             }
         }
+
         val mutations = mutableOldChildren.map { Remove(it, parent) } + mutableNewChildren.map { Insert(it, parent) }
         return SiftResult(mutations, unchangedNodesTuples)
     }
@@ -91,29 +61,10 @@ class TreesDiffManager {
 
     class NodesTuple(val oldNode: CustomNodeInterface, val newNode: CustomNodeInterface)
 
+    class DiffNodeModel<M : Any>(val model: M, val mutations: List<TreeMutation>)
 
-    class DiffNodeModel<M : Any>(model: M, mutations: List<TreeMutation>)
-
-//    fun perform(type: TreeMutation) {
-//        when (type) {
-//            TreeMutation.Insert -> {}
-//            EditOperationType.MOVE -> {
-//                srcNode.parent?.removeChild(srcNode)
-//                dstNode!!.addChild(srcNode, placementIndex)
-//                dstNode.refactorText()
-//            }
-//            EditOperationType.INSERT -> {
-//                dstNode!!.addChild(srcNode, placementIndex)
-//                dstNode.refactorText()
-//            }
-//            EditOperationType.DELETE -> {
-//                val parent = srcNode.parent
-//                parent?.removeChild(srcNode)
-//                parent?.refactorText()
-//            }
-//        }
-//    }
 }
+
 
 sealed class TreeMutation
 
--- src/test/java/TreeTests.kt
+++ src/test/java/TreeTests.kt
@@ -1,8 +1,10 @@
 import abyss.imageManager.CustomIcons
+import abyss.model.tree.diff.TreesDiffManager
 import abyss.model.tree.nodes.NodeModel
 import abyss.model.tree.nodes.TemplateNode
 import javax.swing.Icon
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 import org.junit.Test as test
 
 class TreeTests {
@@ -14,13 +16,31 @@ class TreeTests {
 
     class TestNode(title: String): TemplateNode<TestNodeModel, TestNode, TestNode>(TestNodeModel(title))
 
-    @test fun f() {
-        val root = TestNode("1").including(
+    @test fun `Test root node mutations`() {
+        val sourceTreeRoot = TestNode("1").including(
             TestNode("1-1"),
             TestNode("1-2"),
             TestNode("1-3")
         )
 
-        assertEquals(root.model, TestNodeModel("1"))
+        val resultTreeRoot = TestNode("1").including(
+            TestNode("1-1"),
+            TestNode("1-3"),
+            TestNode("1-4")
+        )
+
+        val mutationsTree = TreesDiffManager().makeMutationsTree(sourceTreeRoot, resultTreeRoot)
+
+        assertNotNull(mutationsTree, "Mutations tree must be non null")
+
+        val rootModel = mutationsTree.userObject as? TreesDiffManager.DiffNodeModel<*>
+
+        assertNotNull(rootModel, "Root mutations must be non null")
+
+        val rootMutations = rootModel.mutations
+
+        assertEquals(rootMutations.size, 2)
+
+        assertEquals(sourceTreeRoot.model, TestNodeModel("1"))
     }
 }
